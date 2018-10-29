e300646149c8dc0709b07165f22391523efe5164
Branislav Holý <branislav@salsitasoft.com>
2018-10-29 15:40:14 +0100

    Updates

---
+++ precache-manifest.05e314ed97abbd3ce8b68219744a6242.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cea7d5892a60cfe201f",
+    "url": "/ws-rabbitmq/static/js/runtime~main.1cea7d58.js"
+  },
+  {
+    "revision": "dff4f7ad708f2cf2ad95",
+    "url": "/ws-rabbitmq/static/js/main.dff4f7ad.chunk.js"
+  },
+  {
+    "revision": "755fe0e00b9747b9d739",
+    "url": "/ws-rabbitmq/static/js/1.755fe0e0.chunk.js"
+  },
+  {
+    "revision": "dff4f7ad708f2cf2ad95",
+    "url": "/ws-rabbitmq/static/css/main.12a218a4.chunk.css"
+  },
+  {
+    "revision": "110d1a9c38f7b74e61f29aea52b3e5e5",
+    "url": "/ws-rabbitmq/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ws-rabbitmq/precache-manifest.a1738ff9198790cda9159dd3207b8fb3.js"
+  "/ws-rabbitmq/precache-manifest.05e314ed97abbd3ce8b68219744a6242.js"
 );
 
 workbox.clientsClaim();
