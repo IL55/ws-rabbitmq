80986c79c51d96652a1b96667c256595fc042df0
Branislav Holý <branislav@salsitasoft.com>
2018-10-29 13:28:57 +0100

    Updates

---
+++ precache-manifest.a1738ff9198790cda9159dd3207b8fb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cea7d5892a60cfe201f",
+    "url": "/ws-rabbitmq/static/js/runtime~main.1cea7d58.js"
+  },
+  {
+    "revision": "8840b907b182ccf80867",
+    "url": "/ws-rabbitmq/static/js/main.8840b907.chunk.js"
+  },
+  {
+    "revision": "50d7134de32b96f49085",
+    "url": "/ws-rabbitmq/static/js/1.50d7134d.chunk.js"
+  },
+  {
+    "revision": "8840b907b182ccf80867",
+    "url": "/ws-rabbitmq/static/css/main.0e85dc3f.chunk.css"
+  },
+  {
+    "revision": "1acb85a9ce6dd6fb0f93f73cab59c977",
+    "url": "/ws-rabbitmq/index.html"
+  }
+];
